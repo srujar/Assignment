6355f18e8370d5eba6cd9e7ea62425b358118e39
Srujan <srujan.kritya@gmail.com>
2021-03-25 17:08:33 +0530

    Added api for Contibutors

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClientCallService } from 'src/app/service/http-client-call.service';
 
 
 @Component({
@@ -9,9 +10,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailsComponent implements OnInit {
 
-  user_data:any;
+  user_data: any;
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private httpClientCallService: HttpClientCallService
   ) { }
 
   ngOnInit(): void {
@@ -19,6 +21,13 @@ export class UserDetailsComponent implements OnInit {
       console.log(data);
       console.log(data.user);
       this.user_data = data.user;
+      this.getContributions(data.user.login);
+    })
+  }
+
+  getContributions(user_name) {
+    this.httpClientCallService.getUserContibutions(user_name).subscribe(data => {
+      console.log("data", data);
     })
   }
 
--- src/app/service/http-client-call.service.ts
+++ src/app/service/http-client-call.service.ts
@@ -7,18 +7,47 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HttpClientCallService {
 
   //Sample Varriables
-  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+  token = '26ee0e844dcfb633ff28174961a8517762b4d499';
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `bearer ${this.token}` }) };
 
   constructor(
     private httpClient: HttpClient,
   ) { }
 
-  getUser(user_name):any{
+  getUser(user_name): any {
     return this.httpClient.get<any>(`https://api.github.com/users/${user_name}`);
   }
 
-  getUsersList():any{
+  getUsersList(): any {
     return this.httpClient.get<any>(`https://api.github.com/users`);
+  }
+
+
+  getUserContibutions(username):any {
+    let body = {
+      "query": `query {
+          user(login: "${username}") {
+            name
+            contributionsCollection {
+              contributionCalendar {
+                colors
+                totalContributions
+                weeks {
+                  contributionDays {
+                    color
+                    contributionCount
+                    date
+                    weekday
+                  }
+                  firstDay
+                }
+              }
+            }
+          }
+        }`
+    }
+    return this.httpClient.post('https://api.github.com/graphql',body,this.httpOptions);
+
   }
 
 }
