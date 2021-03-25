e3bf88c0bd9bdfa649c27e956a78b7ce343038ef
Srujan <admin@Admins-MBP.Dlink>
2021-03-25 19:05:17 +0530

    Added ztest.js

---
+++ ztest.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+getContributions('26ee0e844dcfb633ff28174961a8517762b4d499', 'srujar');
+async function getContributions(token, username) {
+    const headers = {
+        'Authorization': `bearer ${token}`,
+    }
+    const body = {
+        "query": `query {
+            user(login: "${username}") {
+              name
+              contributionsCollection {
+                contributionCalendar {
+                  colors
+                  totalContributions
+                  weeks {
+                    contributionDays {
+                      color
+                      contributionCount
+                      date
+                      weekday
+                    }
+                    firstDay
+                  }
+                }
+              }
+            }
+          }`
+    }
+    const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
+    const data = await response.json()
+    return data
+}
+
+const data = await getContributions('token', 'MeiK2333')
+console.log(data)
